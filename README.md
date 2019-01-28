1430327a5dd7216849fa9067742f78058282cc11
Mohammed Shefeeq <ems.shafeeq@gmail.com>
2019-01-28 13:08:58 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# Group-chat app that uses Nodejs MongoDB and Socket.io
+
+
+
+## Install Dependencies
+```bash
+npm install 
+```
+
+## Run Server
+```bash
+npm start
+```
+
+## Run App
+Open index.html
---
+++ server.js
@@ -0,0 +1,54 @@
+const mongo = require('mongodb').MongoClient;
+const client = require('socket.io').listen(4000).sockets;
+
+// Connect to mongo
+mongo.connect('mongodb://127.0.0.1/Group-Chat', function(err, db){
+    if(err){
+        throw err;
+    }
+
+    console.log('MongoDB connected...');
+
+    // Connect to Socket.io
+    client.on('connection', function(socket){
+        let chat = db.collection('chats');
+
+        // Create function to send status
+        sendStatus = function(s){
+            socket.emit('status', s);
+        }
+
+        // Get chats from mongo collection
+        chat.find().limit(1000).sort({_id:1}).toArray(function(err, res){
+            if(err){
+                throw err;
+            }
+
+            // Emit the messages
+            socket.emit('output', res);
+        });
+
+        // Handle input events
+        socket.on('input', function(data){
+            let name = data.name;
+            let message = data.message;
+
+            // Check for name and message
+            if(message == ''){ //name == '' || 
+                // Send error status
+                sendStatus('Please enter the message');
+            } else {
+                // Insert message
+                chat.insert({name: name, message: message}, function(){
+                    client.emit('output', [data]);
+
+                    // Send status object
+                    sendStatus({
+                        message: 'Message sent',
+                        clear: true
+                    });
+                });
+            }
+        });
+    });
+});
